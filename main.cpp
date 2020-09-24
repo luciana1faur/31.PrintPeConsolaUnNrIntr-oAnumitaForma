7829967ff61bf4f9308ed3314f84bb50cdb064b3
luciana1faur <luciana_faur@yahoo.com>
2020-09-24 12:50:24 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x;
+  cout << "Make a rectangular shape by a single digit number :" << endl;
+  cout << "--------------------------------------" << endl;
+  cout << "Introduceti numarul dumneavoastra: ";
+  cin >> x;
+  cout << x << x << x << x << endl;;
+  cout << x << " " << " " << x << endl;
+  cout << x << " " << " " << x << endl;
+  cout << x << " " << " " << x << endl;;
+  cout << x << " " << " " << x << endl;;
+  cout << x << x << x << x << endl;
+  return 0;
+}
